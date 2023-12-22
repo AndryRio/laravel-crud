a32f91f14fc09c10e22231eeb01bfacd2c6c390a
AndryRio <andreisprincenatu2@gmail.com>
2023-12-22 15:42:30 +0100

    Working Contacts Manager

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [\App\Http\Controllers\ContactController::class, 'index']);
+
+Route::resource('contacts', \App\Http\Controllers\ContactController::class);
